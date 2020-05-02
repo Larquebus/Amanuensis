3e08b7f459b1449e996345db05da5dca102f529f
cwlarabee <chris.larabee9@gmail.com>
2020-05-02 17:44:54 -0500

    Initial commit. Added useful functions for quickly building directory trees based on python iterables.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Amanuensis
+
+This is a repository of useful classes and functions I reuse a lot.
+
+Designed to be installed via pip with:
+```
+pip install git+git://github.com
+```
---
+++ amanuensis.py
@@ -0,0 +1,53 @@
+from os import mkdir
+from os.path import exists
+
+
+def setup_dirs(dir_map: (list, tuple, dict)):
+    """
+    Creates a list of directory paths based on dir_map, which must be
+    an iterable. Then, makes sure each of those directory paths exists,
+    by creating them if necessary.
+
+    :param dir_map: An iterable (list, tuple, or dictionary)
+    :return: None
+    """
+    dir_paths = walk_iter(dir_map)
+    for d in dir_paths:
+        if not exists(d):
+            mkdir(d)
+
+
+def walk_iter(iterable: (list, tuple, dict), prev='', sep='/'):
+    """
+    Unpacks a nested dictionary into a list, with each level of the
+    dictionary represented, so:
+        {'outer': {'innerA': 'A1', 'innerB': ['B1', 'B2']}}
+    becomes:
+        ['outer', 'outer/innerA', 'outer/innerA/A1', 'outer/innerB',
+         'outer/innerB/B1', 'outer/innerB/B2']
+
+    Useful for turning a dictionary mock up of a file structure into
+    an iterable set of file paths.
+
+    :param iterable: An iterable of some kind, either a list, tuple, or
+        dictionary.
+    :param prev: A string, generally only used by walk_iter itself,
+        since this function will call itself recursively.
+    :param sep: A string, the desired separator between keys and values
+        found in the iterable. Default is '/', since it is assumed that
+        this function will be used to generate file paths.
+    :return: A list.
+    """
+    result = []
+    if isinstance(iterable, list) or isinstance(iterable, tuple):
+        for i in iterable:
+            result.append(prev + i)
+    elif isinstance(iterable, dict):
+        for k, v in iterable.items():
+            result.append(prev + k)
+            if isinstance(v, dict) or isinstance(v, list):
+                r = walk_iter(v, prev=prev + k + sep)
+                result += r
+            else:
+                result.append(prev + k + sep + v)
+    return result
