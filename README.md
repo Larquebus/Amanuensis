e4c29fd023a403f14572858e6ac608b03e8036b0
cwlarabee <chris.larabee9@gmail.com>
2020-05-02 19:07:01 -0500

    Rearranged things to create a python package and added a setup file so that it can be installed via git.

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This is a repository of useful classes and functions I reuse a lot.
 
 Designed to be installed via pip with:
 ```
-pip install git+git://github.com
+pip install git+git://github.com/chrislarabee/Amanuensis.git#egg=amanuensis
 ```
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Amanuensis',
+    url='https://github.com/chrislarabee/Amanuensis',
+    author='Chris Larabee',
+    author_email='chris.larabee9@gmail.com',
+    packages=['amanuensis'],
+    install_requires=[],
+    version=0.1,
+    license='Creative Commons',
+    description=('A repository of generalized and generally useful '
+                 'functions and classes I use a lot. Designed to be installed via pip.'),
+    long_description=open('README.md').read()
+)
